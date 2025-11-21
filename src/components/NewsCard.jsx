5b33a29c3fb3b44f02e73d0fae12f59650235508
Babz-G <barbaramgaynor@gmail.com>
2025-11-21 01:17:35 -0500

    added css styling

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,9 +1,18 @@
 function NewsCard({ article }) {
   return (
-    <div>
-      {article.image_url && <img src={article.image_url} alt={article.title} />}
-      <h2>{article.title}</h2>
-      <p>{article.description}</p>
+    <div className="news-card">
+      <h2 className="title">{article.title}</h2>
+      {article.image_url && (
+        <img
+          src={article.image_url}
+          alt={article.title}
+          className="news-image"
+        />
+      )}
+      <p className="description">{article.description}</p>
+      <a href={article.link} className="read-more">
+        Read more
+      </a>
     </div>
   );
 }
